5f47502dd1f0ab3240bc6c8618aed335cb5ba769
guy kalati <guykalati10@gmail.com>
2024-12-11 07:59:06 +0200

    last

--- MyString.java
+++ MyString.java
@@ -65,19 +65,6 @@ public class MyString {
      * @return a string consisting of the characters of str, separated by spaces.
      */
     public static String spacedString(String str) {
-    //     String finalString = "";
-    //     for(int i = 0 ; i < str.length() ; i++){
-    //         if(i == str.length()-1){
-    //             finalString += str.charAt(i);
-    //         }
-    //         else{
-    //             finalString += str.charAt(i);
-    //             finalString += " ";
-    //         }
-    //     }
-    //     return finalString;
-    // }
-
     int length= str.length();
     String newString="";
     if (str.length()==0) {
@@ -121,19 +108,6 @@ public class MyString {
      * @return a string consisting of str1 minus all the characters of str2
      */
     public static String remove(String str1, String str2) {
-    //    String removedString = "";
-    //    String check = "";
-    //    for(int i = 0 ; i < str1.length() ; i++){
-    //     if((str2.indexOf(str1.charAt(i))==-1) || countChar(str2, str1.charAt(i)) == countChar(check, str1.charAt(i))){
-    //         removedString += str1.charAt(i);
-    //     }
-    //     else{
-    //         check += str1.charAt(i);
-            
-    //     }
-    //    }
-    //     return removedString;
-    // }
     char[] newCharArray= new char[str1.length()];
     for (int i=0; i<str1.length(); i++){
         newCharArray[i]=str1.charAt(i);
